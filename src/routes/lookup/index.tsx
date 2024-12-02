6f9e65501caf23004a9afe60d2a98b2a422e533c
Thinh.Nguyen <thinh.nguyen@niteco.se>
2024-12-02 12:26:34 +0700

    fix: logic of lookup function

--- src/routes/lookup/index.tsx
+++ src/routes/lookup/index.tsx
@@ -1,23 +1,23 @@
-import { $, component$, useStore } from "@builder.io/qwik";
+import { $, component$, useComputed$, useSignal } from "@builder.io/qwik";
 import { Link } from "@builder.io/qwik-city";
 import hljs from "highlight.js";
-import type { IQuestion } from "~/data/questions";
 import { questions } from "~/data/questions";
 
 export default component$(() => {
-	const state: { list: IQuestion[] } = useStore({
-		list: []
+	const keyword = useSignal("")
+	const state = useComputed$(() => {
+		return questions.filter(q => q.question.toLowerCase().includes(keyword.value.toLowerCase()))
 	})
 
 	const handleInputChange = $((e: any) => {
-		state.list = questions.filter(q => q.question.includes(e.target.value))
+		keyword.value = e.target.value.toLowerCase()
 	})
 
 	return (
 		<>
 			<section class="bg-slate-900 h-52 overflow-y-auto p-3 flex justify-center items-center relative">
 				<label>
-					<input type="text" onInput$={e => handleInputChange(e)}
+					<input type="text" onKeyUp$={e => handleInputChange(e)}
 						class="text-black py-2 px-3 min-w-96"
 						placeholder="Enter keyword here"
 						autoComplete={"off"}
@@ -28,9 +28,9 @@ export default component$(() => {
 				<Link href="/" class="absolute bottom-3 right-3 underline text-green-400">Go To Test</Link>
 			</section>
 
-			{!!state.list.length &&
+			{!!state.value.length &&
 				<section class="flex flex-col gap-4 p-3 h-full">
-					{state.list.map((q, index) => (
+					{state.value.map((q, index) => (
 						<div key={index} class="p-2 bg-slate-900">
 							<div class="mb-2">{q.question}</div>
 							{q.answerOptions.filter(a => a.isCorrect).map((a, index) => (
